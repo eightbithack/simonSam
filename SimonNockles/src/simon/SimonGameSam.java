06d56980ad467a8d651ac2706dbb529788f16efe
hotfi <hotfi@192.168.1.2>
2017-01-09 23:20:47 -0500

    reset to old simongamesam

--- SimonNockles/src/simon/SimonGameSam.java
+++ SimonNockles/src/simon/SimonGameSam.java
@@ -2,17 +2,17 @@ package simon;
 
 import guiPractice.GUIApplication;
 
-
 public class SimonGameSam extends GUIApplication {
 
 	public SimonGameSam() {
-		
+		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void initScreen() {
-		SimonScreenSam s = new SimonScreenSam(getWidth(), getHeight());
-		setScreen(s);
+	protected void initScreen() {
+		SimonScreenSam work = new SimonScreenSam(getWidth(), getHeight());
+		setScreen(work);
+
 	}
 
 	public static void main(String[] args) {
@@ -20,5 +20,5 @@ public class SimonGameSam extends GUIApplication {
 		Thread app = new Thread(game);
 		app.start();
 	}
-	
+
 }
